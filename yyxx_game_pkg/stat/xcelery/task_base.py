3995a6c07f96335bf21813989b9fe7b0a73668d0
lantianwen <3401858126@qq.com>
2023-04-24 16:20:24 +0800

    feat(task_base):celery task_base trace_id打印

--- yyxx_game_pkg/stat/xcelery/task_base.py
+++ yyxx_game_pkg/stat/xcelery/task_base.py
@@ -8,13 +8,17 @@ celery自定义任务基类
 
 from celery import Task
 
+from yyxx_game_pkg.xtrace.helper import get_current_trace_id
+
 
 class TaskCustomBase(Task):
     def __init__(self):
         self._task_type = "TaskBase"
 
     def on_success(self, retval, task_id, args, kwargs):
+        trace_id = get_current_trace_id()
         print(
+            f"[trace_id: {trace_id}]"
             f'<{ self._task_type}> on_success: {task_id}, kwargs:{kwargs.get("schedule_name")}, '
             f'statistic_id:{kwargs.get("statistic_id")}, svr_id_slice_size:{len(kwargs.get("server_ids", [])),}, '
             f'sdate:{kwargs.get("sdate")}, edate:{kwargs.get("edate")}'
@@ -22,7 +26,9 @@ class TaskCustomBase(Task):
         return super(TaskCustomBase, self).on_success(retval, task_id, args, kwargs)
 
     def on_failure(self, exc, task_id, args, kwargs, einfo):
+        trace_id = get_current_trace_id()
         print(
+            f"[trace_id: {trace_id}]"
             f"<{self._task_type}> on_failure, reason:{exc}, task_id:{task_id}, args:{args}, kwargs:{kwargs}"
         )
         return super(TaskCustomBase, self).on_failure(exc, task_id, args, kwargs, einfo)
