aec3ccae2d1e4cafa4c8831c8e6990fa6c755c6c
winslen <791417205@qq.com>
2023-09-21 16:33:50 +0800

    chore(*):修改logger format的trace_id内容

--- yyxx_game_pkg/logger/config.py
+++ yyxx_game_pkg/logger/config.py
@@ -30,7 +30,7 @@ class LogConfig:
                     "class": "yyxx_game_pkg.logger.formatters.TraceFormatter",
                     "format": (
                         "[%(asctime)s,%(msecs)d: %(levelname)s/%(process)d][%(filename)s:%(funcName)s:%(lineno)d]"
-                        "[%(trace_url)s] %(message)s"
+                        '["trace":"%(trace_id)s"] %(message)s'
                     ),
                 },
             },
